ff83efb678fcbf142771120a73eb3a88906cc172
Osama Kawish <osamakawish@gmail.com>
2020-06-14 12:57:50 -0400

    Initial commit

---
+++ TextBoxes/FancyTextBox.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace TextBoxes
+{
+    partial class FancyTextBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // RichTextBox
+            // 
+            this.RichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RichTextBox.Location = new System.Drawing.Point(0, 0);
+            this.RichTextBox.Name = "RichTextBox";
+            this.RichTextBox.Size = new System.Drawing.Size(800, 450);
+            this.RichTextBox.TabIndex = 0;
+            this.RichTextBox.Text = "";
+            this.RichTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RichTextBox_KeyPress);
+            this.RichTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.RichTextBox_KeyUp);
+            // 
+            // FancyTextBox
+            // 
+            this.Controls.Add(this.RichTextBox);
+            this.Name = "FancyTextBox";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RichTextBox;
+    }
+}
---
+++ TextBoxes/FancyTextBox.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextBoxes
+{
+    /// <summary>
+    /// A text box that allows stronger control of the rich text Box.
+    /// </summary>
+    public partial class FancyTextBox: RichTextBox
+    {
+        private List<TextStyle> TextStyles { get; set; }
+
+        /// <summary>
+        /// The style of the text to be followed by the string in the key, ending with the string in TextStyle.EndsWith. 
+        /// The Color and FontStyle of the text are changed to match the TextStyle.
+        /// </summary>
+        private Dictionary<string,TextStyle> Styles { get; set; }
+
+        /// <summary>
+        /// Use this to make direct edits to the rich text box.
+        /// </summary>
+        public ref RichTextBox TextBox { get { return ref RichTextBox; } }
+
+        public FancyTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public void AddStyle(int priority, string startsWith, string endsWith, FontStyle fontStyle = FontStyle.Regular)
+        {
+            Styles.Add(startsWith,
+                new TextStyle
+                {
+                    Priority = priority,
+                    Color = Color.Black,
+                    FontStyle = fontStyle,
+                    EndsWith = endsWith
+                });
+        }
+
+        public void AddStyle(int priority, string startsWith, string endsWith, Color color, FontStyle fontStyle=FontStyle.Regular)
+        {
+            Styles.Add(startsWith,
+                new TextStyle
+                {
+                    Priority = priority,
+                    Color = color,
+                    FontStyle = fontStyle,
+                    EndsWith = endsWith
+                });
+            UpdateText();
+        }
+
+        /// <summary>
+        /// Modifies the style with the key startsWith to the specified style.
+        /// </summary>
+        /// <param name="startsWith"></param>
+        /// <param name="style"></param>
+        /// <returns>True if a style starts with string startsWith. False otherwise.</returns>
+        public bool ModifyStyle(string startsWith, TextStyle style)
+        {
+            if (!Styles.ContainsKey(startsWith)) return false;
+            
+            Styles[startsWith] = style; UpdateText(); return true;
+        }
+
+        /// <summary>
+        /// Removes the style with the given key.
+        /// </summary>
+        /// <param name="startsWith"></param>
+        /// <returns>True if a style starts with string startsWith. False otherwise.</returns>
+        public bool RemoveStyle(string startsWith)
+        {
+            if (Styles.ContainsKey(startsWith)) { Styles.Remove(startsWith); UpdateText(); return true; }
+            return false;
+        }
+
+        /// <summary>
+        /// Called whenever the styling is modified.
+        /// </summary>
+        private void UpdateText()
+        {
+            string text = RichTextBox.Text;
+            Dictionary<int, int> startEnds = new Dictionary<int, int>();
+
+            // Need to figure out how to do this efficiently.
+            for (int j = 0; j < text.Length; j++)
+            {
+
+            }
+        }
+
+        private void RichTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int i = 0, l = 0, s = RichTextBox.SelectionStart;
+            foreach (var key in Styles.Keys)
+            {
+                l = key.Length; i = s - l;
+                if (i > 0 && RichTextBox.Text.Substring(i,l) == key)
+                {
+                    TextStyles.Add(Styles[key]);
+                }
+            }
+        }
+
+        private void RichTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+
+    public class TextStyle
+    {
+        public int Priority { get; set; }
+        public Color Color { get; set; }
+        public FontStyle FontStyle { get; set; }
+        public string EndsWith { get; set; }
+
+        public static bool operator !=(TextStyle textStyle1, TextStyle textStyle2)
+        {
+            return !(textStyle1 == textStyle2);
+        }
+
+        public static bool operator ==(TextStyle textStyle1, TextStyle textStyle2)
+        {
+            if (textStyle1.Priority != textStyle2.Priority) return false;
+            if (textStyle1.Color != textStyle2.Color) return false;
+            if (textStyle1.FontStyle != textStyle2.FontStyle) return false;
+            if (textStyle1.EndsWith != textStyle2.EndsWith) return false;
+
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this == (TextStyle)obj;
+        }
+
+        public override int GetHashCode()
+        {
+            return Color.ToArgb() + Priority + FontStyle.GetHashCode() + EndsWith.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{Priority}: {Color} {FontStyle} (Ends With: {EndsWith})";
+        }
+    }
+}
