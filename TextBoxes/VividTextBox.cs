59f9cb8963c70641d7d851dc6de11eb2b369b664
Osama Kawish <osamakawish@gmail.com>
2020-06-15 12:14:22 -0400

    Completed VividTextBox for testing. Removed TagTextBox from project. Built project.

--- TextBoxes/VividTextBox.cs
+++ TextBoxes/VividTextBox.cs
@@ -11,152 +11,209 @@ using System.Windows.Forms;
 namespace TextBoxes
 {
     /// <summary>
-    /// A text box that allows stronger control of the rich text Box.
+    /// A text box that allows direct control of the RichTextBox.
     /// </summary>
     public partial class VividTextBox: RichTextBox
     {
-        private List<TextStyle> TextStyles { get; set; }
-
         /// <summary>
-        /// The style of the text to be followed by the string in the key, ending with the string in TextStyle.EndsWith. 
-        /// The Color and FontStyle of the text are changed to match the TextStyle.
+        /// 
         /// </summary>
-        private Dictionary<string,KeyValuePair<string,TextStyle>> Styles { get; set; }
-
-        public VividTextBox()
+        /// <param name="fontStyle"></param>
+        /// <returns>Text style based on the color of the currently selected text and the given font style.</returns>
+        public TextStyle GetTextStyle(FontStyle fontStyle)
         {
-            this.KeyPress += KeyPressEvent;
-            this.KeyUp += KeyUpEvent;
-        }
-
-        public void AddStyle(int priority, string startsWith, string endsWith, FontStyle fontStyle = FontStyle.Regular)
-        {
-            Styles.Add(startsWith, new KeyValuePair<string, TextStyle> (endsWith,
-                new TextStyle
-                {
-                    Priority = priority,
-                    Color = Color.Black,
-                    FontStyle = fontStyle
-                }));
-        }
-
-        public void AddStyle(int priority, string startsWith, string endsWith, Color color, FontStyle fontStyle=FontStyle.Regular)
-        {
-            Styles.Add(startsWith, new KeyValuePair<string, TextStyle> (endsWith,
-                new TextStyle
-                {
-                    Priority = priority,
-                    Color = color,
-                    FontStyle = fontStyle
-                }));
-            UpdateText();
+            return new TextStyle
+            {
+                Color = SelectionColor,
+                FontStyle = fontStyle
+            };
         }
 
         /// <summary>
-        /// Modifies the style with the key startsWith to the specified style.
+        /// 
         /// </summary>
-        /// <param name="startsWith"></param>
+        /// <param name="color"></param>
+        /// <returns>Text style based on the font style of the currently selected text and the provided color.</returns>
+        public TextStyle GetTextStyle(Color color)
+        {
+            return new TextStyle
+            {
+                Color = color,
+                FontStyle = SelectionFont.Style
+            };
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="fontStyle"></param>
+        /// <returns>Text style based on the given font style and color.</returns>
+        public static TextStyle GetTextStyle(Color color, FontStyle fontStyle)
+        {
+            return new TextStyle
+            {
+                Color = color,
+                FontStyle = fontStyle
+            };
+        }
+
+        /// <summary>
+        /// Applies the color to the current selection.
+        /// </summary>
+        /// <remarks>This is more efficient than defining a text style and applying it as it applies the color directly.</remarks>
+        /// <param name="color"></param>
+        public void ApplyStyle(Color color)
+        {
+            SelectionColor = color;
+        }
+
+        /// <summary>
+        /// Applies the font style to the current selection. 
+        /// </summary>
+        /// <remarks>This is slightly more efficient than defining a text style and applying it, as it applies the font style directly.</remarks>
+        /// <param name="fontStyle"></param>
+        public void ApplyStyle(FontStyle fontStyle)
+        {
+            Font font = SelectionFont;
+            SelectionFont = new Font(font, fontStyle);
+        }
+
+        /// <summary>
+        /// Applies the given color and style to the text.
+        /// </summary>
+        /// <remarks>This does not create a new text style.</remarks>
+        /// <param name="color"></param>
+        /// <param name="fontStyle"></param>
+        public void ApplyStyle(Color color, FontStyle fontStyle)
+        {
+            SelectionColor = color;
+            Font ft = SelectionFont;
+            SelectionFont = new Font(ft, fontStyle);
+        }
+
+        /// <summary>
+        /// Styles the characters in the selection with the given style.
+        /// </summary>
         /// <param name="style"></param>
-        /// <returns>True if a style starts with string startsWith. False otherwise.</returns>
-        public bool ModifyStyle(string startsWith, string endsWith, TextStyle style)
+        public void ApplyStyle(TextStyle style)
         {
-            if (!Styles.ContainsKey(startsWith)) return false;
-            
-            Styles[startsWith] = new KeyValuePair<string, TextStyle>(endsWith, style); UpdateText(); return true;
-        }
-
-        /// <summary>
-        /// Removes the style with the given key.
-        /// </summary>
-        /// <param name="startsWith"></param>
-        /// <returns>True if a style starts with string startsWith. False otherwise.</returns>
-        public bool RemoveStyle(string startsWith)
-        {
-            if (Styles.ContainsKey(startsWith)) { Styles.Remove(startsWith); UpdateText(); return true; }
-            return false;
-        }
-
-        /// <summary>
-        /// Called whenever the styling is modified.
-        /// </summary>
-        private void UpdateText()
-        {
-            Dictionary<int, HashSet<string>> keyLengths = new Dictionary<int, HashSet<string>>();
-            // Organize dict keys by string length
-            foreach (var key in Styles.Keys) {
-                var l = key.Length;
-                if (keyLengths.ContainsKey(l))
-                {
-                    keyLengths[l].Add(key);
-                }
-                else
-                {
-                    keyLengths.Add(l, new HashSet<string> { key });
-                }
-            } 
-
-            // Need to account for Style Priority
-            for (int textPos = 0; textPos < Text.Length; textPos++)
-            {
-                foreach (var len in keyLengths.Keys)
-                {
-                    if (len < textPos) UpdateTextStyle(textPos, len, keyLengths);
-                }
-            }
-        }
-
-        private void UpdateTextStyle(int textPos, int len, Dictionary<int, HashSet<string>> keyLengths)
-        {
-            foreach (var str in keyLengths[len])
-            {
-                if (Text.Substring(0, len).EndsWith(str))
-                {
-                    // Style the text until the end string is reached
-                    StyleSelection(textPos - len, textPos, Styles[str].Value);
-                }
-            }
-
-        }
-
-        private void KeyPressEvent(object sender, KeyPressEventArgs e)
-        {
-            int s = this.SelectionStart;
-
-            foreach (var key in Styles.Keys)
-            {
-                int l = key.Length;
-                int i = s - l;
-                if (i > 0 && this.Text.Substring(i,l) == key)
-                {
-                    // DO NOT REMOVE. Line added for readability.
-                    TextStyle style = Styles[key].Value;
-                    
-                    TextStyles.Add(style);
-                }
-            }
-        }
-
-        public void StyleSelection(int start, int end, TextStyle style)
-        {
-            int s = SelectionStart, l = SelectionLength;
-
-            Select(start, end - start);
-            SelectionColor = style.Color;
+            SelectionColor = style.Color; 
             Font ft = SelectionFont;
             SelectionFont = new Font(ft, style.FontStyle);
+        }
 
+        /// <summary>
+        /// Applies the given color to the selected text. 
+        /// </summary>
+        /// <remarks>If selection length is 0, then the whole word containing the caret position is colored.</remarks>
+        public void StyleSelection(Color color)
+        {
+            if (SelectionLength > 0)
+            {
+                ApplyStyle(color);
+            }
+            else
+            {
+                int s = SelectionStart, l = SelectionLength;
+                SelectWholeWord(); ApplyStyle(color);
+                Select(s, l);
+            }
+        }
+
+        /// <summary>
+        /// Applies the given font style to the selected text.
+        /// </summary>
+        /// <remarks>If selection length is 0, then the font style is applied to the whole word containing the caret position.</remarks>
+        /// <param name="fontStyle"></param>
+        public void StyleSelection(FontStyle fontStyle)
+        {
+            if (SelectionLength > 0)
+            {
+                ApplyStyle(fontStyle);
+            }
+            else
+            {
+                int s = SelectionStart, l = SelectionLength;
+                SelectWholeWord(); ApplyStyle(fontStyle);
+                Select(s, l);
+            }
+        }
+
+        /// <summary>
+        /// Styles the currently selected characters according to the given style.
+        /// </summary>
+        /// <remarks>If the selection length is 0, it styles the set of alphanumeric characters containing the selection start.</remarks>
+        /// <param name="style"></param>        
+        public void StyleSelection(TextStyle style) 
+        {
+            if (SelectionLength > 0)
+            {
+                ApplyStyle(style);
+            }
+            else
+            {
+                int s = SelectionStart, l = SelectionLength;
+                SelectWholeWord(); ApplyStyle(style);
+                Select(s, l);
+            }
+        }
+
+        /// <summary>
+        /// Colors the text in the provided index range.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="length"></param>
+        /// <param name="color"></param>
+        public void StyleSelection(int start, int length, Color color)
+        {
+            int s = SelectionStart, l = SelectionLength;
+            Select(start, length); ApplyStyle(color);
             Select(s, l);
         }
 
-        private void KeyUpEvent(object sender, KeyEventArgs e)
+        /// <summary>
+        /// Applies the font style to the text in the provided index range.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="length"></param>
+        /// <param name="fontStyle"></param>
+        public void StyleSelection(int start, int length, FontStyle fontStyle)
         {
+            int s = SelectionStart, l = SelectionLength;
+            Select(start, length); ApplyStyle(fontStyle);
+            Select(s, l);
+        }
 
+        /// <summary>
+        /// Styles the characters within the given index range.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="style"></param>
+        public void StyleSelection(int start, int length, TextStyle style)
+        {
+            int s = SelectionStart, l = SelectionLength;
+            Select(start, length); ApplyStyle(style); Select(s, l);
+        }
+
+        /// <summary>
+        /// Selects the whole word(s) containing the current selection.
+        /// </summary>
+        /// <remarks>A whole word is a consecutive set of letters and digits.</remarks>
+        public void SelectWholeWord()
+        {
+            while (SelectionStart > 0 && char.IsLetterOrDigit(Text[SelectionStart])) SelectionStart--;
+
+            for (int pos = SelectionStart + SelectionLength; char.IsLetterOrDigit(Text[pos]) && pos < Text.Length; pos++)
+            {
+                SelectionLength++;
+            }
         }
     }
 
     public class TextStyle
     {
-        public int Priority { get; set; }
         public Color Color { get; set; }
         public FontStyle FontStyle { get; set; }
 
@@ -167,7 +224,6 @@ namespace TextBoxes
 
         public static bool operator ==(TextStyle textStyle1, TextStyle textStyle2)
         {
-            if (textStyle1.Priority != textStyle2.Priority) return false;
             if (textStyle1.Color != textStyle2.Color) return false;
             if (textStyle1.FontStyle != textStyle2.FontStyle) return false;
 
@@ -181,12 +237,12 @@ namespace TextBoxes
 
         public override int GetHashCode()
         {
-            return Color.ToArgb() * Priority * FontStyle.GetHashCode();
+            return Color.ToArgb() * FontStyle.GetHashCode();
         }
 
         public override string ToString()
         {
-            return $"{Priority}: {Color} {FontStyle}";
+            return $"TextStyle({Color}, {FontStyle})";
         }
     }
 }
